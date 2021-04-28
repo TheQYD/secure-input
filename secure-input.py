69676200a370f0c5c7456070943039ded81fad0d
Qasim Dove <emailqasim@gmail.com>
2021-04-27 23:45:38 -0400

    Fixed some minor bugs with the typing.

--- secure-input.py
+++ secure-input.py
@@ -1,15 +1,27 @@
 import getch
+import keyboard
 
 def secureInput(prompt='', mask=''):
   output = ''
-  print(prompt, end='', flush=True)
+  #print(prompt, end='', flush=True)
 
   while True:
+
+    print('\r' + prompt, end='', flush=True)
     keyboard_input = getch.getch()
+    
     if keyboard_input == '\n':
       break
-    print(mask, end='', flush=True)
-    output += keyboard_input
+    
+    elif keyboard_input == '2':
+      output = output[:-1]
+    
+    else:
+      output += keyboard_input
+    
+
+    #print(mask, end='', flush=True)
+    #output += keyboard_input
   return ('\n' + output)
 
 
@@ -17,4 +29,4 @@ if __name__ == '__main__':
   username = str(input("Enter your username: "))
   password = str(secureInput("Enter your password: ", '*'))
 
-  print(password)
+  print('\n' + password)
